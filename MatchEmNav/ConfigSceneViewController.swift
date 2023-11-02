72602634e9f9a47a68f05c1be7d2657648789bef
dsarell1 <dsarell1@kent.edu>
2023-11-02 17:11:48 -0400

    Fixed Speed. Started working on TableView.

--- MatchEmNav/ConfigSceneViewController.swift
+++ MatchEmNav/ConfigSceneViewController.swift
@@ -4,16 +4,30 @@
 //
 //  Created by Dylan Sarell on 10/29/23.
 //
+/*
+let GameManager = [
+    "Dasnay": 20,
+    "Kevin": 17,
+    "Sarah": 23
+]*/
+struct HighScore {
+    var name: String
+    var score: Int
+}
 
 import UIKit
 
-class ConfigSceneViewController: UIViewController {
-
+class ConfigSceneViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     var gameSceneVC: GameSceneViewController?
+    
+    @IBOutlet weak var highScoreTable: UITableView!
     
     @IBOutlet weak var SpeedSlider: UISlider!
     @IBOutlet weak var bigRecToggle: UISwitch!
     @IBOutlet weak var gameTimerStepper: UIStepper!
+    
+    var data: [HighScore]?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -21,8 +35,10 @@ class ConfigSceneViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.highScoreTable.delegate = self
+        self.highScoreTable.dataSource = self
+        self.highScoreTable.rowHeight = 50
+        self.data = [HighScore(name: "Sarah", score: 23), HighScore(name: "Dasnay", score: 20), HighScore(name: "Kevin", score: 17)]
     }
     override func viewWillAppear(_ animated: Bool) {
         SpeedSlider.value = Float(1 / (gameSceneVC?.newRectInterval)!)
@@ -30,15 +46,20 @@ class ConfigSceneViewController: UIViewController {
         gameTimerStepper.value = Double((gameSceneVC?.timeRemaining)!)
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
     @IBAction func SpeedSilderAction(_ sender: UISlider) {
-        self.newRectTimer?.invalidate()
+        gameSceneVC?.newRectTimer?.invalidate()
         var rate = 1.0 / Double(SpeedSlider.value)
-        //gameSceneVC?.newRectInterval = Double(SpeedSlider.value)
         gameSceneVC?.newRectInterval = rate
-        self.newRectTimer = Timer.scheduledTimer(withTimeInterval: gameSceneVC?.newRectInterval, repeats: true, block: { Timer in
-            if self.gameRunning == true {
-                self.createButton()
-                self.matchCount += 1
+        
+        gameSceneVC?.newRectTimer = Timer.scheduledTimer(withTimeInterval: (gameSceneVC?.newRectInterval)!, repeats: true, block: { [self] Timer in
+            if gameSceneVC?.gameRunning == true {
+                gameSceneVC?.createButton()
+                gameSceneVC?.matchCount += 1
             }
         })
     }
@@ -51,15 +72,5 @@ class ConfigSceneViewController: UIViewController {
         gameTimerStepper.maximumValue = 60
         gameSceneVC?.timeRemaining = Int(gameTimerStepper.value)
     }
-    
-    /*
-     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
